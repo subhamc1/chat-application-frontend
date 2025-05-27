69202c62e080076150a2a12c08ec108dfe3c3288
subhamc1 <f20212702@goa.bits-pilani.ac.in>
2025-05-27 13:17:11 +0530

    Triggering github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/chat-application-frontend/',
   plugins: [react(), tailwindcss()],
 })
